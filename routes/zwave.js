1978d9c027c2a6f3f5d971bbfb5235a8d34a08ec
Rene Bosch <r.bosch@nerds.company>
2016-03-05 12:29:52 +0100

    add scss lint + style conventions

--- routes/zwave.js
+++ routes/zwave.js
@@ -1,43 +1,42 @@
-require( "dotenv" ).load();
+require('dotenv').load();
 
 const OZW = require('openzwave-shared');
 
 const Zwave = (req, res, io) => {
+  const zwave = new OZW({
+    Logging: false,
+  });
 
-    const zwave = new OZW({
-        Logging: false,
+  const nodeList = [];
+
+  zwave.connect(process.env.ZWAVE_USB);
+
+  zwave.on('node added', (nodeid) => {
+    console.log('node added: ', nodeid);
+  });
+
+  zwave.on('node ready', (nodeid, nodeinfo) => {
+    console.log('node naming: ', nodeid, nodeinfo);
+
+    nodeinfo['nodeid'] = nodeid;
+
+    nodeList.push(nodeinfo);
+  });
+
+  zwave.on('scan complete', () => {
+    console.log('== SCAN COMPLETE');
+
+    res.render('zwave', {
+      title: 'Zwave page title',
+      nodeCollection: nodeList,
     });
+  });
 
-    const nodeList = [];
-
-    zwave.connect(process.env.ZWAVE_USB);
-
-    zwave.on('node added', (nodeid) => {
-        console.log('node added: ', nodeid);
+  io.on('connection', (socket) => {
+    socket.on('update-zwave-list', (message) => {
+      console.log('UPDATE LIST !!!!!!1');
     });
-
-    zwave.on('node ready', (nodeid, nodeinfo) => {
-        console.log('node naming: ', nodeid, nodeinfo);
-
-        nodeinfo['nodeid'] = nodeid;
-
-        nodeList.push(nodeinfo);
-    });
-
-    zwave.on('scan complete', () => {
-        console.log('== SCAN COMPLETE');
-
-        res.render('zwave', {
-            title: "Zwave page title",
-            nodeCollection: nodeList,
-        });
-    });
-
-    io.on('connection', (socket) => {
-        socket.on('update-zwave-list', (message) => {
-            console.log('UPDATE LIST !!!!!!1');
-        });
-    });
+  });
 
 }
 
